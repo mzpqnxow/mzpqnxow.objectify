9ec945c763e277bca0e3eac4d9f47eede01e5238
Adam Greene <copyright@mzpqnxow.com>
2020-02-14 15:35:35 -0500

    Update test driver

--- test.py
+++ test.py
@@ -2,10 +2,12 @@
 from collections import OrderedDict
 from sys import argv, stderr
 
+from ujson import dumps
+
 from objectify.xml import objectify_xml
 from objectify.yaml import objectify_yaml
 from objectify.json import objectify_json
-from objectify.io import objectify_open
+from objectify.io import objectify_read, objectify_write
 
 
 def main():
@@ -13,23 +15,24 @@ def main():
     infile = argv[1]
 
     loadmap = {
-        '.json': objectify_json,
-        '.yaml': objectify_yaml,
-        '.yml': objectify_yaml,
-        '.xml': objectify_xml
+        '.json': (objectify_json, {}),
+        '.yaml': (objectify_yaml, {}),
+        '.yml': (objectify_yaml, {'template': True}),
+        '.xml': (objectify_xml, {})
     }
 
-    buf = objectify_open(infile, readbuf=True)
     obj = None
-    for extension, handler in loadmap.items():
+    for extension, handler_args in loadmap.items():
         if infile.endswith(extension):
+            handler = handler_args[0]
+            kwargs = handler_args[1]
             outfile = infile.replace(extension, '.json')
             # Each handler takes a string as input, not a filename
             # Each handler returns a native Python3 datatype, either
             # a dict or OrderedDict. Both types can be cleanly and
             # safely serialized to JSON, or just used in memory as a
             # standard dict
-            obj = handler(buf)
+            obj = handler(infile, **kwargs)
             break
     else:
         stderr.write('Unknown extension!\n')
@@ -42,7 +45,8 @@ def main():
         exit(1)
 
     # Write JSON
-    objectify_open(outfile, 'w', json_object=obj)
+    print(dumps(obj, indent=2))
+    objectify_write(outfile, obj, as_json=True)
     print('Conversion complete, please see {} ...'.format(outfile))
 
 
